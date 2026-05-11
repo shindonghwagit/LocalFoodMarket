dd8c1a0160b61d07db537ec9cec7b75e1becb567
shindonghwagit <ek6511011@naver.com>
2026-05-11 16:32:33 +0900

    Spring Boot 초기 세팅 및 DB 연결 설정

---
+++ backend/src/main/java/com/localfood/localfoodmarket/LocalfoodmarketApplication.java
@@ -0,0 +1,14 @@
+package com.localfood.localfoodmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalfoodmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalfoodmarketApplication.class, args);
+	}
+
+}
+
---
+++ backend/src/test/java/com/localfood/localfoodmarket/LocalfoodmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localfood.localfoodmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalfoodmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
